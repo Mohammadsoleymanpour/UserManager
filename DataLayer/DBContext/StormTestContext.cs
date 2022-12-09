7888d0d46cf8cc6083bd55a057a3fcf2e046a4c7
Mohammad Hosein Soleymanpour <111628438+Mohammadsoleymanpour@users.noreply.github.com>
2022-12-09 10:43:23 +0330

    Create Context And ConnectionString

---
+++ DataLayer/DBContext/StormTestContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.DBContext
+{
+    public class StormTestContext:DbContext
+    {
+        public StormTestContext(DbContextOptions<StormTestContext> options):base(options)
+        {
+            
+        }
+
+        #region DbSets
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasQueryFilter(u => !u.IsDelete);
+        }
+
+    }
+}
--- StormTest/Program.cs
+++ StormTest/Program.cs
@@ -1,3 +1,6 @@
+using DataLayer.DBContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+  #region ConnectionString
+
+
+builder.Services.AddDbContext<StormTestContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+#endregion
 
 var app = builder.Build();
 
